bf4bd764fe0ad4cca2635d77908057ce94549abc
Mehta, Devraj <Devraj.Mehta@gtri.gatech.edu>
2014-08-08 12:29:35 -0400

    more keybindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -18,12 +18,12 @@ myConfig = gnomeConfig { modMask = mod4Mask -- use the super key
 
 myKeys = [ ("M-g", goToSelected defaultGSConfig)
          , ("M-l", spawn "gnome-screensaver-command -l")
-         , ("M-o", spawn "dmenu_run")
-         , ("M-S-o", spawn "gmrun")
+         , ("M-o", spawn "gmrun")
          , ("M-b", spawn "google-chrome")
          , ("M-f", spawn "firefox")
          , ("M-c", spawn "pidgin")
          , ("M-x", kill)
          , ("M-v", spawn "/opt/cisco/anyconnect/bin/vpnui")
+         , ("M-S-v", spawn "VirtualBox")
          ]
 
