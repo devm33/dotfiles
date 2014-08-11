16730adf5146705ddc59a63f38ecf1a89909c048
Mehta, Devraj <Devraj.Mehta@gtri.gatech.edu>
2014-08-11 10:59:53 -0400

    stole victors xmonad.hs customizations also change terminal key to resolve conflict with default

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Layout.NoBorders (noBorders)
+import XMonad.Layout.Reflect
 import XMonad.Hooks.ManageDocks
 import XMonad.Actions.GridSelect
 import XMonad.Util.EZConfig (additionalKeysP)
@@ -10,10 +11,13 @@ main = xmonad myConfig
 myConfig = gnomeConfig { modMask = mod4Mask -- use the super key
                          , terminal = "gnome-terminal"
 
-                         -- remove all borders, dont cover gnome-panel 
-                         , layoutHook = noBorders $ avoidStruts $ layoutHook defaultConfig
-                         -- dont tile gnome-panel
-                         , manageHook = manageDocks
+                         -- remove borders, uncover gnome-panel, custom layout
+                         , layoutHook = noBorders $ avoidStruts $ myLayout
+
+                         -- modded manage hook
+                         , manageHook = myManageHook <+> manageHook gnomeConfig
+
+                         -- custom key bindings
                          } `additionalKeysP` myKeys
 
 myKeys = [ ("M-g", goToSelected defaultGSConfig)
@@ -21,10 +25,20 @@ myKeys = [ ("M-g", goToSelected defaultGSConfig)
          , ("M-o", spawn "gmrun")
          , ("M-b", spawn "google-chrome")
          , ("M-f", spawn "firefox")
-         , ("M-t", spawn "gnome-terminal")
+         , ("M-1", spawn "gnome-terminal")
          , ("M-c", spawn "pidgin")
          , ("M-x", kill)
          , ("M-v", spawn "/opt/cisco/anyconnect/bin/vpnui")
          , ("M-m", spawn "VirtualBox")
          ]
 
+myLayout = tiled ||| reflectHoriz tiled ||| Mirror tiled ||| Full
+    where
+        tiled = Tall nmaster delta ratio
+        nmaster = 1
+        ratio = 1/2
+        delta = 3/100
+
+myManageHook = composeAll [
+                          -- className =? "Pidgin"  --> doFloat
+                          ]
