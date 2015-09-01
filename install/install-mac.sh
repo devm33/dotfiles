f4d477437c64a13c17700945cc38ecc2f4eb87c1
Devraj Mehta <devm33@gmail.com>
2015-09-01 15:14:08 -0400

    Update and rename install-mac.sh to osx.sh

--- install/install-mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script to download, setup, and install deps for dotfiles
-# OSX version
-# v0 - totally untested
-
-# TODO
---
+++ install/osx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script to download, setup, and install deps for dotfiles
+# OSX version
+# v0 - totally untested
+
+# before running ensure that there is a valid ssh key authorized for github
+
+cd $HOME
+
+git clone git@github.com:devm33/dotfiles.git .dotfiles
+
+git clone git@github.com:robbyrussell/oh-my-zsh.git .oh-my-zsh
+
+# Note: version encoded here will become stale!
+curl -LO https://thoughtbot.github.io/rcm/dist/rcm-1.2.3.tar.gz && \
+tar -xvf rcm-1.2.3.tar.gz && \
+cd rcm-1.2.3 && \
+./configure && \
+make && \
+sudo make install
+
+cd $HOME
+ln -s .dotfiles/rcrc .rcrc
+rcup -v
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install node
+brew install cmake
