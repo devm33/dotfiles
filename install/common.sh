2a51de8b85932da127b208249658d4b6831d97d5
Devraj Mehta <devm33@gmail.com>
2016-05-08 17:26:22 -0400

    broke out common code in common.sh, improved host selection

---
+++ install/common.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script to download, setup, and install deps for dotfiles
+# Common components
+
+cd $HOME
+
+git clone git@github.com:devm33/dotfiles.git .dotfiles
+
+git clone git@github.com:robbyrussell/oh-my-zsh.git .oh-my-zsh
+
+# Note: version here will become stale!
+RCMV='1.3.0'
+curl -LO https://thoughtbot.github.io/rcm/dist/rcm-$RCMV.tar.gz && \
+tar -xvf rcm-$RCMV.tar.gz && \
+cd rcm-$RCMV && \
+./configure && \
+make && \
+sudo make install
+
+cd $HOME
+
+{ for f in .dotfiles/host-*; do echo $f; done } | cut -d- -f2
+echo -n 'Select the hostname to use (defaults to personal): '
+read host
+
+if [ ! -d ".dotfiles/host-$host" ]; then
+    host='personal'
+fi
+ln -s ".dotfiles/host-$host/rcrc" .rcrc
+rcup -v
--- install/osx.sh
+++ install/osx.sh
@@ -2,39 +2,14 @@
 
 # Script to download, setup, and install deps for dotfiles
 # OSX version
-# v0 - totally untested
 
 # before running ensure that there is a valid ssh key authorized for github
 
-cd $HOME
+# Install common components
 
-git clone git@github.com:devm33/dotfiles.git .dotfiles
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/devm33/dotfiles/master/install/common.sh)"
 
-git clone git@github.com:robbyrussell/oh-my-zsh.git .oh-my-zsh
-
-# Note: version encoded here will become stale!
-curl -LO https://thoughtbot.github.io/rcm/dist/rcm-1.2.3.tar.gz && \
-tar -xvf rcm-1.2.3.tar.gz && \
-cd rcm-1.2.3 && \
-./configure && \
-make && \
-sudo make install
-
-cd $HOME
-echo -n 'Enter w to use hostname work instead of default'
-read host
-if [ "$host" == "w" ]; then
-    echo -n 'Enter m for gmac otherwise using goobuntu'
-    read os
-    if [ "$os" == "m" ]; then
-        ln -s .dotfiles/host-gmac/rcrc .rcrc
-    else
-        ln -s .dotfiles/host-work/rcrc .rcrc
-    fi
-else
-    ln -s .dotfiles/host-personal/rcrc .rcrc
-fi
-rcup -v
+# OSX Specific post install
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
