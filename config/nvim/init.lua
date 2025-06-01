291392312cf128e18fd95d7f27c0da290cb5acaa
Devraj Mehta <devm33@github.com>
2025-06-01 11:19:13 -0400

    Relative number

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,16 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = "a"
