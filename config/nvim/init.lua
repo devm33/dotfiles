11cb231184af1d41e6ddcc10b651539768d6a7d3
Devraj Mehta <devm33@github.com>
2026-03-01 23:28:50 -0500

    turn on ls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -523,12 +523,7 @@ require("lazy").setup({
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
-                -- Some languages (like typescript) have entire language plugins that can be useful:
-                --    https://github.com/pmizio/typescript-tools.nvim
-                --
-                -- But for many setups, the LSP (`ts_ls`) will work just fine
-                -- ts_ls = {},
-                --
+                ts_ls = {},
 
                 lua_ls = {
                     -- cmd = { ... },
