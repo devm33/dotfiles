ab29c4b7def17518c1201cca668e0b1cfe36ad9d
Devraj Mehta <devm33@github.com>
2026-07-28 22:57:13 -0400

    rust

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -546,9 +546,9 @@ require("lazy").setup({
                 -- clangd = {},
                 -- gopls = {},
                 -- pyright = {},
-                -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
+                rust_analyzer = {},
                 ts_ls = {},
 
                 lua_ls = {
