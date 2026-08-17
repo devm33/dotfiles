1d8d04fed3e0c7e8f04f2236d918ab2abd0c56b7
Devraj Mehta <devm33@github.com>
2026-08-17 20:44:20 +0000

    more install

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -6,8 +6,14 @@
 # before running ensure that there is a valid ssh key authorized for github
 
 # Ubuntu/Debian Specific install
-sudo apt-get install --yes cmake tmux silversearcher-ag nodejs npm neovim fzf ripgrep
+sudo apt-get install --yes cmake tmux silversearcher-ag nodejs npm neovim fzf ripgrep unzip
 sudo npm i -g npm
+
+# Install Rust and Cargo
+if ! command -v cargo >/dev/null 2>&1; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+[ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
 
 # Install common components
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/devm33/dotfiles/main/install/common.sh)" || exit
